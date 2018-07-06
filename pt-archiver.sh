e2620c80bb05466f3a5e4ed8a90467b720827424
applegit11 <13774437301@163.com>
2018-07-06 19:05:23 +0800

    Update pt-archiver.sh

--- pt-archiver.sh
+++ pt-archiver.sh
@@ -59,9 +59,9 @@ EXEC_DATE=`date +%Y-%m-%d`
 TABLE_DATE=`date +%Y%m%d` 
 FILE_DIR=`dirname $0` 
 FILE_NAME=`basename $0 .sh` 
-USER="root" 
-PASSWORD="123456"
-HOST="1.1.1.10" 
+USER="zyadmin" 
+PASSWORD="Zhuyun@123"
+HOST="rm-uf6blk06co539g17n.mysql.rds.aliyuncs.com" 
 
 #主表归档  
 sed '/^#.*\|^$/d' ${FILE_DIR}/${FILE_NAME}.def >${FILE_DIR}/${FILE_NAME}.tmp 
@@ -94,18 +94,17 @@ do
 	echo " pt-archiver  --source h=${HOST},P=3306,u=${USER},p=${PASSWORD},D=${SOURCE_DB},t=${TABLE_NAME} --dest h=${HOST},P=3306,u=${USER},p=${PASSWORD},D=${DEST_DB},t=${HISTABLE_NAME} --charset=UTF8 --where '${FLITER_FIELD} < unix_timestamp(\""${EXEC_DATE}" 00:00:00\") and ${FLITER_FIELD} != 2016' --progress 5000 --limit=1000 --txn-size=1000 --statistics --bulk-insert  --bulk-delete --no-delete --no-version-check ">pt-archiver-incre-${TABLE_NAME}.sh 
 	bash pt-archiver-incre-${TABLE_NAME}.sh >pt-archiver-incre-${TABLE_NAME}.log
 ## 备表归档 
-	#echo `mysql -h${HOST} -u${USER} -p${PASSWORD} -e "select id from ${DEST_DB}.${HISTABLE_NAME};" | sed -n '2,$p'` > master_id_file
+	if [ ${TABLE_NAME} = 'vgp_payment' ] ; then
+	echo `mysql -h${HOST} -u${USER} -p${PASSWORD} -e "select b.id from ${DEST_DB}.${HISTABLE_NAME} a,${DEST_DB}.${TABLE_NAME_JOIN} b where a.id = b.payment_id" | sed -n '2,$p'` > master_id_file
+	else
 	echo `mysql -h${HOST} -u${USER} -p${PASSWORD} -e "select b.id from ${DEST_DB}.${HISTABLE_NAME} a,${DEST_DB}.${TABLE_NAME_JOIN} b where a.id = b.order_id" | sed -n '2,$p'` > master_id_file
- 
+ 	fi
+
 	for line in $(cat master_id_file)
 	do
-		#echo " pt-archiver  --source h=${HOST},P=3306,u=${USER},p=${PASSWORD},D=${SOURCE_DB},t=${TABLE_NAME_JOIN} --dest h=${HOST},P=3306,u=${USER},p=${PASSWORD},D=${DEST_DB},t=${HISTABLE_NAME_JOIN} --charset=UTF8 --where '${FLITER_FIELD_JOIN}=${line}' --progress 5000 --limit=1000 --txn-size=1000 --statistics --bulk-insert --bulk-delete --no-delete --no-version-check ">pt-archiver-incre-${TABLE_NAME_JOIN}.sh 
-		echo " pt-archiver  --source h=${HOST},P=3306,u=${USER},p=${PASSWORD},D=${SOURCE_DB},t=${TABLE_NAME_JOIN} --dest h=${HOST},P=3306,u=${USER},p=${PASSWORD},D=${DEST_DB},t=${HISTABLE_NAME_JOIN} --charset=UTF8 --where '=${line}' --progress 5000 --limit=1000 --txn-size=1000 --statistics --bulk-insert --bulk-delete --no-delete --no-version-check " >pt-archiver-incre-${TABLE_NAME_JOIN}.sh
-	bash pt-archiver-incre-${TABLE_NAME_JOIN}.sh >pt-archiver-incre-${TABLE_NAME_JOIN}.log ${FLITER_FIELD_JOIN}
+		echo " pt-archiver  --source h=${HOST},P=3306,u=${USER},p=${PASSWORD},D=${SOURCE_DB},t=${TABLE_NAME_JOIN} --dest h=${HOST},P=3306,u=${USER},p=${PASSWORD},D=${DEST_DB},t=${HISTABLE_NAME_JOIN} --charset=UTF8 --where '${FLITER_FIELD_JOIN}=${line}' --progress 5000 --limit=1000 --txn-size=1000 --statistics --bulk-insert --bulk-delete --no-delete --no-version-check ">pt-archiver-incre-${TABLE_NAME_JOIN}.sh 
+	bash pt-archiver-incre-${TABLE_NAME_JOIN}.sh >pt-archiver-incre-${TABLE_NAME_JOIN}.log 
 	done
 done 
 exit 0
 
-cat pt-archiver.def 
-##源数据库名,源主表名,筛选字段,目标库名,备表归档筛选字段,备表名,备表筛选字段
-db,t,updateTime,db,id,t_join,order_id
